1d437d66cef04621077cd0aac6badd4d13c56e9d
WannabeBotter <WannabeCryptoBotter@gmail.com>
2023-04-17 23:42:41 +0000

    デバッグ実行時のためにパス設定をするようにした

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+import sys
 import pygui_util
 
 if __name__ == '__main__':
+    _source_file_path = os.path.abspath(sys.argv[0]) 
+    _source_directory = os.path.dirname(_source_file_path)
+    os.chdir(_source_directory)
+
     pygui_util.pygui_init()
     pygui_util.pygui_destroy()
